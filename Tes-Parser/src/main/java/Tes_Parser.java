1747e451d84130497b3e39cddc79b88fa8562a23
ShFsn <tigr.tigrov.1977@gmail.com>
2022-11-29 16:43:59 +0300

    Add javadocs and commentary to Tes_Parser.java

--- Tes-Parser/src/main/java/Tes_Parser.java
+++ Tes-Parser/src/main/java/Tes_Parser.java
@@ -2,42 +2,62 @@ import java.util.Scanner;
 import org.w3c.dom.Document;
 
 
-
+/**
+ * Main class that contatin initializing sequence and Container class for shared data use.
+ * @author Ivan Strebkov
+ * @see Tes_Parser.Container Shared use data
+ * @see Tes_Parser#main(String[]) Start point
+ * @see Tes_Parser#InitSequence1(Container) Initializing sequence 1
+ * @see Tes_Parser#InitSequence2(Container) Initializing sequence 2
+ * */
 public class Tes_Parser {
+    /**
+     * A Container class which is used to share data
+     * @author Ivan Strebkov
+     *  */
     public static class Container{
         String filepath = null;
-        Document document = null;
-        int doc_type = 0;
+        Document document = null;   // DOM tree is here.
+        int doc_type = 0;           // -1 - Unrecognizable data; 0 - Empty file; 1 - Terms; 2 - Publications.
     }
 
 
-
+    /**
+     * Start point of the program. Starts 2 initializing sequencies and gives the control to the one of two file menus.
+     * @author Ivan Strebkov
+     * @param args - Parameters from user
+     * @see Tes_Parser#InitSequence1(Container) Initializing sequence 1
+     * @see Tes_Parser#InitSequence2(Container) Initializing sequence 2
+     * @see TERMS#Terms_main(Container) Main terms menu
+     * @see PUBLICATIONS#Publications_main(Container) Main publications menu
+     * @see Tes_Parser.Container Shared use data
+     */
     public static void main(String[] args) {
         int ret;
 
-        //Создаются "глобальные" переменные для отправки в методы
+        // Shared data "variables" are being created.
         Container container = new Container();
 
-        //Инициализирующая последовательность
+        // Initializing.
         do {
-            //Получение и считывание файла
+            // Receiving and loading the file.
             ret = InitSequence1(container);
-            if (ret == 1) { //Была возвращена команда на выход
+            if (ret == 1) { // Exit code was returned.
                 return;
             }
 
-            //Определяем тип данных: термины или литература
+            // Determining data type: Terms or Publications.
             ret = InitSequence2(container);
-            if (ret == 1) { //Была возвращена команда на выход
+            if (ret == 1) { // Exit code was returned.
                 return;
-            } else if (ret == 0) {  //Была возвращена команда на смену файла
+            } else if (ret == 0) {  // File successfully determined.
                 break;
             }
         }
-        while (true);
+        while (true); // In case ret == -1 Initializing starts again (file change).
 
-        //Конец инициализирующей последовательности
-        //Передача управления В соответствующую функцию менеджмента
+        // End of initializing.
+        // Giving the control to corresponding management class.
         switch (container.doc_type) {
             case 1 -> TERMS.Terms_main(container);
             case 2 -> PUBLICATIONS.Publications_main(container);
@@ -45,17 +65,27 @@ public class Tes_Parser {
     }
 
 
-
+    /**
+     * Initializing sequence #1 that select, load and create (if needed) the resulting xml file.
+     * @author Ivan Strebkov
+     * @param container Shared use data
+     * @return Returns the result of file loading:
+     * 0 - file loaded;
+     * 1 - file not loaded and exit must be executed.
+     * @see Tes_Parser.Container Shared use data
+     * @see FILE#FileSelect(Container) File path and name inserter
+     * @see FILE#FileLoad(Container) File loader
+     */
     public static int InitSequence1(Container container) {          //Получение и считывание файла
         Scanner in = new Scanner(System.in);
         container.document = null;
 
         do {
-            //Считывается путь до файла
+            // Reading file path.
             FILE.FileSelect(container);
-            //Создаётся (при надобности) и загружается файл
+            // Creating (if needed) and loading the file.
             FILE.FileLoad(container);
-            if (container.document == null) {
+            if (container.document == null) {           // document remains null if no file loaded.
                 label:
                 while (true) {
                     System.out.println("\nRetry with another path or Quit program? (r|q):");
@@ -64,11 +94,11 @@ public class Tes_Parser {
                     switch (ans) {
                         case "r":
                         case "R":
-                            //Цикл прерывается и выполнение идёт заново
+                            // Brake the cycle for the new path reading.
                             break label;
                         case "q":
                         case "Q":
-                            //Выход
+                            // Exit program.
                             return 1;
                         default:
                             System.out.print("\nIncorrect input. Try again.");
@@ -82,20 +112,30 @@ public class Tes_Parser {
     }
 
 
-
-    public static int InitSequence2(Container container) {          //Определяем тип данных: термины или литература
+    /**
+     * Initializing sequence #2 that determines the type of the loaded file.
+     * @author Ivan Strebkov
+     * @param container Shared use data
+     * @return Returns the result of file type determination sequence:
+     * -1 - file type not determined and file must be changed;
+     * 0 - file type determined;
+     * 1 - file type not determined and exit must be executed.
+     * @see Tes_Parser.Container Shared use data
+     * @see DOM#GetDOMType(Document) Xml tree type recognition
+     */
+    public static int InitSequence2(Container container) {
         Scanner in = new Scanner(System.in);
 
         int doc_type = DOM.GetDOMType(container.document);
         if (doc_type > 0) {
-            //По содержимому файла удалось определить тип
+            // File type successfully determined.
             switch (doc_type) {
                 case 1 -> System.out.print("\nFile type: Terms.\n");
                 case 2 -> System.out.print("\nFile type: Publications.\n");
             }
             container.doc_type = doc_type;
         } else if (doc_type == 0) {
-            //Файл пуст
+            // File is empty.
             label:
             while (true) {
                 System.out.print("""
@@ -119,7 +159,7 @@ public class Tes_Parser {
                 }
             }
         } else {
-            //Файл непуст и содержит неизвестные данные
+            // File is not empty but contains unexpected data.
             while (true) {
                 System.out.print("\nFile contains unknown data.\nRetry with another path or Quit program? (r|q):\n");
                 String ans = in.next();
@@ -127,11 +167,11 @@ public class Tes_Parser {
                 switch (ans) {
                     case "r":
                     case "R":
-                        //Цикл прерывается и выполнение идёт заново
+                        // Initialization must be done again (file change).
                         return -1;
                     case "q":
                     case "Q":
-                        //Выход
+                        // Exit program.
                         return 1;
                     default:
                         System.out.println("\nIncorrect input. Try again.");
@@ -139,6 +179,6 @@ public class Tes_Parser {
                 }
             }
         }
-        return 0;
+        return 0; // Successful completion.
     }
 }
